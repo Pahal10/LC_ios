edf1101382d86659db82711a5a0f9ba350405863
Pahal10 <130741070+Pahal10@users.noreply.github.com>
2026-08-13 17:03:32 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'dart:convert';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
